31ff428c40cbe8cdc4fbb9548815279e586ba32b
wangzz2019 <jack.wang@datadoghq.com>
2021-02-04 22:14:33 +0900

    add a spa test

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,10 @@ router.get('/', function(req, res, next) {
 router.get('/promise',function(req,res,next){
   res.render('promise',{title:"promise test page"});
 });
+router.get('/spa',function(req,res,next){
+  msg="jackmsg";
+  res.send(msg);
+});
 
 router.get('/pizzaorder',function(req,res,next){
   res.render('pizzaorder',{});
@@ -53,7 +57,7 @@ router.post('/webhook',function(req,res,next){
   orgname=req.body.org.name;
   var d=new Date();
   var dt=d.toLocaleString();
-  console.log(dt + "  event_title is " + eventtitle);
+  //console.log(dt + "  event_title is " + eventtitle);
   returnmsg={"body":eventmsg,"title":eventtitle,"orgid":orgid,"orgname":orgname};
   console.log(returnmsg);
   res.send(JSON.stringify(returnmsg));
@@ -158,68 +162,6 @@ router.get('/test',function(req, res, next){
     response={"id":1,"name":"wangzz"}
     res.send(JSON.stringify(response));
 });                                       
-
-router.get('/imagesearch/addpic', function (req, res, next) {
-  var Client = require("@alicloud/imagesearch-2018-01-20");
-  var client = new Client({
-    accessKeyId: "***********",  // 获取地址: https://ak-console.aliyun.com
-    accessKeySecret: "***************",
-    endpoint: "http://***.com", // 获取地址: https://help.aliyun.com/document_detail/66616.html
-    apiVersion: "2018-01-20"
-
-  });
-  var instanceName = "tesuto"; // 购买的图像搜索实例名称，如: imagesearchtest
-  var catId = "0"; // 类目ID，参考: https://help.aliyun.com/document_detail/66623.html
-  var itemId = "1234"; // 商品/图片ID
-  var custContent = "{\"key\":\"value\"}"; // 商品/图片的Meta信息，搜索结果中会透传
-  var picList = {}; // 图片列表
-  // 下面以当前目录下的1.jpg 和 2.jpg 为例
-  picList[new Buffer("1.jpg").toString("base64")] = fs.readFileSync(path.resolve(__dirname, "../public/images/bag01.jpg")).toString("base64");
-  picList[new Buffer("2.jpg").toString("base64")] = fs.readFileSync(path.resolve(__dirname, "../public/images/bag02.jpg")).toString("base64");
-  var buildAddContent = function () {
-    if (!itemId || !catId || Object.keys(picList).length <= 0) {
-      return;
-    }
-    if (custContent == null) {
-      custContent = "";
-    }
-    var params = {};
-    params.item_id = itemId;
-    params.cat_id = catId + "";
-    params.cust_content = custContent;
-    var picListStr = "";
-    Object.keys(picList).forEach(function (picName) {
-      if (picList[picName].length <= 0) {
-        return;
-      }
-      picListStr += picName + ",";
-      params[picName] = picList[picName];
-    });
-    params.pic_list = picListStr.substr(0, picListStr.length - 1);
-    return buildContent(params);
-  }
-  var buildContent = function (params) {
-    var meta = "";
-    var body = "";
-    var start = 0;
-    Object.keys(params).forEach(function (key) {
-      if (meta.length > 0) {
-        meta += "#";
-      }
-      meta += key + "," + start + "," + (start + params[key].toString().length);
-      body += params[key];
-      start += params[key].length;
-    })
-    return meta + "^" + body;
-  }
-  client.addItem({
-    instanceName: instanceName,
-  }, buildAddContent()).then(function (value) {
-    console.log("Result", JSON.stringify(value));
-  }).catch(function (err) {
-    console.log("Error Message: ", err);
-  });
-});
 
 function getIPAdress() {
   var interfaces = require('os').networkInterfaces();　　
