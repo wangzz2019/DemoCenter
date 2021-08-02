598a37bccabf4132c33257e3e5cb8618b0d2298c
wangzz2019 <jack.wang@datadoghq.com>
2021-08-02 21:36:54 +0900

    add bad request for num1

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,40 @@
 var express = require('express');
 var router = express.Router();
-var fs=require('fs');
-var path=require('path');
+var fs = require('fs');
+var path = require('path');
 var df = require('dateformat');
-const {exec}=require('child_process');
+const { exec } = require('child_process');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   //res.render('pizza42', { title: 'Pizza 42' });
-  res.render('index',{title:'Home Page'});
+  res.render('index', { title: 'Home Page' });
 });
-router.get('/testdiv', function(req, res, next) {
+router.get('/testdiv', function (req, res, next) {
   //res.render('pizza42', { title: 'Pizza 42' });
-  res.render('testdiv',{title:'testdiv'});
+  res.render('testdiv', { title: 'testdiv' });
 });
-router.get('/promise',function(req,res,next){
-  res.render('promise',{title:"promise test page"});
+router.get('/video', function (req, res, next) {
+  //res.render('pizza42', { title: 'Pizza 42' });
+  res.render('video', { title: 'video' });
 });
-router.get('/spa',function(req,res,next){
+router.get('/promise', function (req, res, next) {
+  res.render('promise', { title: "promise test page" });
+});
+router.get('/spa', function (req, res, next) {
   // response={"id":1,"name":"wangzz"}
   // res.send(JSON.stringify(response));
   res.render("spa");
 });
 
-router.get('/pizzaorder',function(req,res,next){
-  res.render('pizzaorder',{});
+router.get('/pizzaorder', function (req, res, next) {
+  res.render('pizzaorder', {});
 });
 
-router.post('/reboot',function(req,res,next){
+router.post('/reboot', function (req, res, next) {
   //run a local shell command for test
-  command="ls -al";
-  exec(command,(err,stdout,stderr)=>{
+  command = "ls -al";
+  exec(command, (err, stdout, stderr) => {
     if (err) {
       console.log(err);
       return;
@@ -40,142 +44,147 @@ router.post('/reboot',function(req,res,next){
   })
 });
 
-router.get('/localtest',function(req,res,next){
-  testString={ items: [ '06:28:44 UTC', 'centos72' ], message: 'test message3' };
+router.get('/localtest', function (req, res, next) {
+  testString = { items: ['06:28:44 UTC', 'centos72'], message: 'test message3' };
   console.log(testString.message);
 });
 
-router.post('/webhook',function(req,res,next){
-  msg="Hi, this is a return page of POST test";
-  eventtitle=req.body.title;
+router.post('/webhook', function (req, res, next) {
+  msg = "Hi, this is a return page of POST test";
+  eventtitle = req.body.title;
   // console.log("======starting print whole request body============");
   // console.log(req.body);
   // console.log("======print end====================================");
-  title=eventtitle.split(' ').pop();
-  if (title=="host1") {
+  title = eventtitle.split(' ').pop();
+  if (title == "host1") {
     //start host2
     console.log("Oh, it is host1, let's reboot jackTest");
     // command='ssh -i /root/AlibabaTokyo.pem root@192.168.1.134 "reboot"';
-    exec(command,(err,stdout,stderr)=>{
-    if (err) {
-      //console.log(err);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-  })
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        //console.log(err);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    })
   }
-  eventmsg=req.body.body;
-  orgid=req.body.org.id;
-  orgname=req.body.org.name;
-  alert_type=req.body.alert_type;
-  alert_status=req.body.alert_status;
-  log_sample=req.body.log_sample;
+  eventmsg = req.body.body;
+  orgid = req.body.org.id;
+  orgname = req.body.org.name;
+  alert_type = req.body.alert_type;
+  alert_status = req.body.alert_status;
+  log_sample = req.body.log_sample;
 
-  if (log_sample == null){
-    log_sample="";
+  if (log_sample == null) {
+    log_sample = "";
   }
-  else{
-    rows=log_sample.rows;
+  else {
+    rows = log_sample.rows;
     console.log("Printing the sample logs here:");
-    for (i=0;i<rows.length;i++){
+    for (i = 0; i < rows.length; i++) {
       console.log(rows[i].message);
     }
   }
   console.log("");
   // console.log(log_sample);
-  var d=new Date();
-  var dt=d.toLocaleString();
+  var d = new Date();
+  var dt = d.toLocaleString();
   //console.log(dt + "  event_title is " + eventtitle);
-  returnmsg={"body":eventmsg,"title":eventtitle,"orgid":orgid,"orgname":orgname,"ALERT_TYPE":alert_type,"ALERT_STATUS":alert_status,"LOG_SAMPLE":log_sample};
+  returnmsg = { "body": eventmsg, "title": eventtitle, "orgid": orgid, "orgname": orgname, "ALERT_TYPE": alert_type, "ALERT_STATUS": alert_status, "LOG_SAMPLE": log_sample };
   // returnmsg={"body":eventmsg,"orgid":orgid,"orgname":orgname,"ALERT_TYPE":alert_type,"ALERT_STATUS":alert_status,"LOG_SAMPLE":log_sample};
   console.log(returnmsg);
   console.log(JSON.stringify(req.headers));
   res.send(JSON.stringify(returnmsg));
 });
 
-router.post('/test',function(req,res,next){
-  msg="Hi, this is a return page of POST test";
-  eventtitle=req.body.event_title;
-  var d=new Date();
-  var dt=d.toLocaleString();
+router.post('/test', function (req, res, next) {
+  msg = "Hi, this is a return page of POST test";
+  eventtitle = req.body.event_title;
+  var d = new Date();
+  var dt = d.toLocaleString();
   console.log(dt + "  event_title is " + eventtitle);
   res.send(msg + " and the event title is " + eventtitle);
 });
 
 
 
-router.post('/multistep',function(req,res,next){
-  pattern=req.body.pattern;
+router.post('/multistep', function (req, res, next) {
+  pattern = req.body.pattern;
   var response;
-  if (pattern==1){
-    response={"ResultCode":"0100","ResultMessage":"resultmessages1"}
+  if (pattern == 1) {
+    response = { "ResultCode": "0100", "ResultMessage": "resultmessages1" }
   }
-  else if (pattern==2){
-    response={"ResultCode":"0200","ResultMessage":"resultmessages2"}
+  else if (pattern == 2) {
+    response = { "ResultCode": "0200", "ResultMessage": "resultmessages2" }
   }
   else {
-    response={"ResultCode":"0300","ResultMessage":"resultmessages3"}
+    response = { "ResultCode": "0300", "ResultMessage": "resultmessages3" }
   }
   res.send(JSON.stringify(response));
 });
 
-router.post('/num1',function(req,res,next){
-  var1=req.body.num1;
-  res.send(var1);
+router.post('/num1', function (req, res, next) {
+  var1 = req.body.num1;
+  if (var1 == undefined) {
+    res.status(400).json({status:400, message:"bad request"});
+  }
+  else {
+    res.send(var1);
+  }
   console.log(var1);
 });
 
-router.post('/bodytest',function(req,res,next){
-  var1=req.body.customerOccurDate;
-  var2=req.body.customerOccurTime;
-  var3=req.body.orderInfos[0].orderNumber;
+router.post('/bodytest', function (req, res, next) {
+  var1 = req.body.customerOccurDate;
+  var2 = req.body.customerOccurTime;
+  var3 = req.body.orderInfos[0].orderNumber;
   //msg="hi, this is page for testing request body";
   console.log(var1);
   console.log(var2);
   console.log(var3);
-  response={"result":"0010","result1":"0000"};
+  response = { "result": "0010", "result1": "0000" };
   res.send(JSON.stringify(response));
 });
 
-router.get('/showip',function(req,res,next) {
-  var serverIP=getIPAdress();
+router.get('/showip', function (req, res, next) {
+  var serverIP = getIPAdress();
   //var clientIP=req.ip
-  msg="Server IP is: " + serverIP;
-  msg+="<br />Client IP is: " + req.ip.match(/\d+\.\d+\.\d+\.\d+/);
+  msg = "Server IP is: " + serverIP;
+  msg += "<br />Client IP is: " + req.ip.match(/\d+\.\d+\.\d+\.\d+/);
   res.send(msg);
 });
 
-router.get('/spantag',function(req,res,next){
+router.get('/spantag', function (req, res, next) {
   //scope.activate(() => {
-    const tracer = require('dd-trace').init();
-    const span = tracer.startSpan('web.request');
-    
-    span.setTag('http.url', '/spantag');
-    span.setTag('ip','192.168.1.1');
-    
-    
-    msg="This is a page for span tag test, please check the tag info on Datadog"
-    span.finish();
-    res.send(msg);
-    
+  const tracer = require('dd-trace').init();
+  const span = tracer.startSpan('web.request');
+
+  span.setTag('http.url', '/spantag');
+  span.setTag('ip', '192.168.1.1');
+
+
+  msg = "This is a page for span tag test, please check the tag info on Datadog"
+  span.finish();
+  res.send(msg);
+
   //});
 });
 
-function epoctotime(epoctime){
-  var epoct=parseFloat(epoctime);
-  var UTCDate=df(new Date(epoct),'yyyymmddHH:MM:ss',false);
+function epoctotime(epoctime) {
+  var epoct = parseFloat(epoctime);
+  var UTCDate = df(new Date(epoct), 'yyyymmddHH:MM:ss', false);
   //var day=df()
   return UTCDate;
 }
 
-router.get('/epoc',function(req,res,next){
-  retVal=epoctotime(1580986533000);
+router.get('/epoc', function (req, res, next) {
+  retVal = epoctotime(1580986533000);
   //res.send(retVal.toLocaleTimeString('en-US', {timeZone: 'Asia/Tokyo'}));
   res.send(retVal);
 });
 
-router.post('/epoc',function(req,res,next){
+router.post('/epoc', function (req, res, next) {
   /*{
     "body": "$EVENT_MSG",
     "last_updated": "$LAST_UPDATED",
@@ -189,44 +198,44 @@ router.post('/epoc',function(req,res,next){
     "id": "$ID"
 }*/
 
-  last_updated=req.body.last_updated;
-  epoc="last_updated in EPOC is " + last_updated;
+  last_updated = req.body.last_updated;
+  epoc = "last_updated in EPOC is " + last_updated;
   console.log(epoc);
-  readableTime="last_update in YYYYDDMMHHSSmm is " + epoctotime(last_updated);
+  readableTime = "last_update in YYYYDDMMHHSSmm is " + epoctotime(last_updated);
   console.log(readableTime);
   res.send(epoc + '<br>' + readableTime);
 });
 
-router.post('/docomotest',function(req,res,next){
-  testdata=req.body.testdata;
+router.post('/docomotest', function (req, res, next) {
+  testdata = req.body.testdata;
   console.log(testdata);
-  res.setHeader('content-type',"application/json");
-  msg={"id":1,"msg":testdata};
+  res.setHeader('content-type', "application/json");
+  msg = { "id": 1, "msg": testdata };
   res.send(JSON.stringify(msg));
 });
 
-router.get('/test',function(req, res, next){
-    res.setHeader('Access-Control-Allow-Origin','http://127.0.0.1:3000');
-    res.setHeader('Last-Modified', (new Date()).toUTCString());
-    res.setHeader('content-type',"application/json");
-    s="Hi are you ok";
-    // sp=s.split(' ');
-    // console.log(s.split(' ').pop());
-    //var args=URL.parse(req.url,true).query;
-    response={"id":1,"name":"wangzz"}
-    res.send(JSON.stringify(response));
-});                                       
+router.get('/test', function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+  res.setHeader('Last-Modified', (new Date()).toUTCString());
+  res.setHeader('content-type', "application/json");
+  s = "Hi are you ok";
+  // sp=s.split(' ');
+  // console.log(s.split(' ').pop());
+  //var args=URL.parse(req.url,true).query;
+  response = { "id": 1, "name": "wangzz" }
+  res.send(JSON.stringify(response));
+});
 
 function getIPAdress() {
-  var interfaces = require('os').networkInterfaces();　　
-  for (var devName in interfaces) {　　　　
-      var iface = interfaces[devName];　　　　　　
-      for (var i = 0; i < iface.length; i++) {
-          var alias = iface[i];
-          if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
-              return alias.address;
-          }
-      }　　
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
   }
 }
 
